e34b85592bce713531c03ec75633127395bc46b1
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-03-12 18:49:50 +0000

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am Osita Nduka.
         </p>
         <a
           className="App-link"
