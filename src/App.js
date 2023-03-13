71c005cc86279c2cf760827b192a7c2d1d9a5c1a
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-03-13 21:14:01 +0000

    modified app.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Home from './Home';
 import Projects from './Projects';
 import Contact from './Contact';
 import Navbar from './components/Navbar';
-import {Routes, Route, Link} from 'react-router-dom'; 
+import {Routes, Route, Link, Router} from 'react-router-dom'; 
 
 function App() {
   return (
-    <div>
-    <Navbar />
+    <Router>
+     <div>
+       <Navbar />
       
         <Routes>
            <Route path='/' element={<Home />} />
@@ -17,8 +18,8 @@ function App() {
            <Route path='/Projects' element={<Projects />} />
         </Routes>
       
-    </div>
-    
+      </div>
+    </Router>
   );
 }
 
