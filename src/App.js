bcf67c3c3ca401a931bfc0b3f03210e167db39a9
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-03-14 12:51:48 +0000

    fix src

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
-import Home from './Home';
-import Projects from './Projects';
-import Contact from './Contact';
-import Navbar from './components/Navbar';
+
+
  
 
 function App() {
