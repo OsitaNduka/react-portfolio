990387ac40e85d27cd761c4732913fbd5a8fafdf
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-04-21 19:55:59 +0100

    modified footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import LinkedInIcon from "@mui/material/LinkedIn";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>Footer
+    <div> 
+        <LinkedInIcon />
+        <githubIcon />
+    </div>
+    <p> &copy; 2023 ositanduka.com</p>
+    </div>
   )
 }
 
