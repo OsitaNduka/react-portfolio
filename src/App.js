816a09e1af4b25334e34d52148011788ab0ec569
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-04-21 17:21:41 +0100

    added pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './Home';
-import Projects from './Projects';
-import Contact from './Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 import Navbar from './components/Navbar';
 
 
