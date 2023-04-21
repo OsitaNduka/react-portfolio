62a807787d1a7e19b7355c0ee05a2ac2f74328eb
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-04-21 17:32:54 +0100

    deleted index.css

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Contact from './Contact';
-import Projects from './Projects';
-import Home from './Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
   
-  <Router>
-    <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home/>}/>
-          <Route path='/Contact' element={<Contact/>}/>
-          <Route path='/Projects' element={<Projects/>}/>
-        </Route>
-    </Routes>
-  </Router>,
 );
 
 // If you want to start measuring performance in your app, pass a function
