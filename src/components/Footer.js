d3cc7d7d96cc606b3cc17d98d3974eae909aad61
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-04-21 21:25:55 +0100

    added icons to footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { GitHub } from '@mui/icons-material'
+import { LinkedIn } from '@mui/icons-material'
 
 function Footer() {
   return (
     <div className='footer'>
     <div> 
-        
+     <GitHub /> 
+     <LinkedIn />  
     </div>
     <p> &copy; 2023 ositanduka.com</p>
     </div>
