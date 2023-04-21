a7ecb24c56b1eb9677380b5702d1aa0c2b29d03a
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-04-21 20:19:07 +0100

    deleted material icons in footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import LinkedInIcon from "@mui/icons-material";
 
 function Footer() {
   return (
     <div className='footer'>Footer
     <div> 
-        <LinkedInIcon />
-        <githubIcon />
+        
     </div>
     <p> &copy; 2023 ositanduka.com</p>
     </div>
