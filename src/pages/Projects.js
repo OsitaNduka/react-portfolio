f13a6c4c85390badea076b454c252c6bfc1b0432
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-04-23 14:24:31 +0100

    added boostrap portfolio to project.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    {
+      title: 'Bootsrap Portfolio',
+      image: './components/assets/Boostrap screenshot.png',
+      description: 'Bootstrap Portfolio',
+      github: 'https://github.com/OsitaNduka/Bootstrap-Portfolio'
+    }
   )
 }
 
