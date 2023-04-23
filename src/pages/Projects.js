1f69f7f4e9c94e6f1a5d94d344f460381864e6f4
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-04-23 16:16:13 +0100

    added images to projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,38 @@
+import { Container } from '@mui/material'
 import React from 'react'
 
 const Projects = () => {
-  return (
+
+  const projects = [
     {
-      title: 'Bootsrap Portfolio',
-      image: './components/assets/Boostrap screenshot.png',
-      description: 'Bootstrap Portfolio',
-      github: 'https://github.com/OsitaNduka/Bootstrap-Portfolio'
+      title: 'Bootstrap Portfolio',
+      description: 'Bootstrap Portfolio profile',
+      imgUrl: 'bootstrapImg',
+    },
+    {
+      title: 'Css-Grid Layout',
+      description: 'Search Engine Optimization',
+      imgUrl: 'css-gridImg'
+    },
+    {
+      title: 'Travel-App',
+      description: 'Travel App Tracker',
+      imgUrl: 'travel-appImg',
     }
+
+    ]
+  
+  return (
+   <section className='projects' id='projects'>
+    <Container>
+      <Row>
+        <Col>
+        <h2>Projects</h2>
+        <p>My Projects display is wonderful</p>
+        </Col>
+      </Row>
+    </Container>
+   </section>
   )
 }
 
