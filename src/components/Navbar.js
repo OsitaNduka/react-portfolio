5370475ac4665e646c18890de314090e688057da
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-04-25 18:07:54 +0100

    modified navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,6 @@ const Navbar = () => {
             <Link to='/'>Home</Link>
             <Link to='/projects'>Projects</Link>
             <Link to='/contact'>Contact</Link>
-            <Link to='/projectcard'>ProjectCard</Link>
             </div>
         </div>
   );
