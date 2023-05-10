7b881dd9de4b51039563519ae9ecd05ac5af9903
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-05-10 13:42:36 +0100

    added helpers/ProjectList.js

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,29 @@
+import bootstrap from "../assets/"
+
+export const projectList = [
+    {
+      name: 'Bootstrap Portfolio',
+      image: 'Bootstrap Portfolio profile',
+      skills: 'bootstrap',
+    },
+    {
+      name: 'Css-Grid Layout',
+      image: 'Search Engine Optimization',
+      skills: 'HoriseonImg'
+    },
+    {
+      name: 'Travel-App',
+      imgUrl: 'Travel-AppImg',
+      description: 'Travel App Tracker',
+      
+    },
+    
+    {
+       name: 'Travel-App',
+       imgUrl: 'Travel-AppImg',
+       description: 'Travel App Tracker',
+        
+      
+    },
+
+    ]
