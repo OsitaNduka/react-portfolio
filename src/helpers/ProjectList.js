87d51a13962c0e50bba902d43370a296e5827470
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-05-15 19:29:24 +0100

    modified projectlist.js

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -7,16 +7,19 @@ import positiveVibes from "../components/assets/positiveVibes.png";
 
 export const ProjectList = [
     {
+      id: 0,
       name: 'Bootstrap Portfolio',
       image: bootstrap,
       skills: 'My Bootstrap Portfolio',
     },
     {
+      id: 1,
       name: 'Css-Grid Layout',
       image: horiseon,
       skills: 'Search Engine Optimization',
     },
     {
+      id: 2,
       name: 'Travel-App',
       image: travelApp,
       skills: 'Travel App Tracker',
@@ -24,19 +27,20 @@ export const ProjectList = [
     },
     
     {
+       id: 3,
        name: 'Password Generator',
        imgage: passwordGen,
-       skills: 'A Password Generator',
-        
-      
+       skills: 'A Password Generator', 
     },
-    {
+      {
+        id: 4,
         name: 'A profile website',
         image: bioImage,
         skills: 'A profile website',
         
       },
       {
+        id: 5,
         name: 'Positive Vibes',
         image: positiveVibes,
         skills: 'A feel good Website',
